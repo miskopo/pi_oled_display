a62e4dbf1b37ca679f69248de5585cadb3048b7a
Michal Polovka <michal.polovka@gmail.com>
2018-11-12 14:32:19 +0100

    CurrentWeather screen

---
+++ README.md
@@ -0,0 +1,2 @@
+## Raspberry Pi OLED display viewer
+
--- pi_oled_display/arg_parser.py
+++ pi_oled_display/arg_parser.py
@@ -5,4 +5,7 @@ parser = ArgumentParser()
 parser.add_argument("--image",
                     nargs=1,
                     help="Path to image to show")
+parser.add_argument("--group",
+                    nargs=1,
+                    help="Group of screens to show")
 
