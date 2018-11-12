24a07c5b1344626df8e108cb722950874ae73f6d
Michal Polovka <michal.polovka@gmail.com>
2018-11-12 12:47:01 +0100

    CurrentWeather screen

---
+++ pi_oled_display/screens/current_weather.py
@@ -0,0 +1,20 @@
+from requests import get
+
+
+class CurrentWeather:
+    __slots__ = 'url', 'city'
+
+    def __init__(self, city=None):
+        self.url = "http://wttr.in/"
+        self.city = city
+
+    def obtain_weather(self):
+        try:
+            with get("{}{}".format(self.url, self.city if not None else "")) as response:
+                if response.ok:
+                    return "\n".join(response.text.split("\n")[1:7])
+                else:
+                    raise ConnectionError
+        except ConnectionError:
+            # Unable to connect
+            return "Unable to connect"
